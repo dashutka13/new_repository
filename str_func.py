0cd49f1121cf70250e9455ef3f1ccdab95e3d550
dashuta <it.dashutka@gmail.com>
2023-06-06 01:18:22 +0300

    add text_title

--- str_func.py
+++ str_func.py
@@ -1,5 +1,19 @@
 def text_upper():
+    """
+    Все заглавные буквы
+    :return:
+    """
     text = input("Ввод: \n")
     new_text = text.upper()
     return new_text
+
+
+def text_title():
+    """
+    заглавные буквы первые
+    """
+    text = input("Ввод: \n")
+    new_text = text.title()
+    return new_text
+
 
